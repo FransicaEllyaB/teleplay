20374e877fd452ad68be044dc9c41bc446f45c06
FransiscaEllyaB <ellyafransiscab@gmail.com>
2024-09-17 18:23:41 +0700

    PUSH PWS

--- teleplay/settings.py
+++ teleplay/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://fransisca-ellya-teleplay.pbp.cs.ui.ac.id/", "http://fransisca-ellya-teleplay.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/fransisca.ellya/teleplay", "https://pbp.cs.ui.ac.id/fransisca.ellya/teleplay"]
